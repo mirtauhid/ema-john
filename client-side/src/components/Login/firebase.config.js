bc27e04ad151b2d23db23d18b0571154d3534dd8
mirtauhid <67647821+mirtauhid@users.noreply.github.com>
2020-10-09 01:35:42 +0600

    firebase added

--- client-side/src/components/Login/firebase.config.js
+++ client-side/src/components/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBHhOX8rtp2vahVcATkhTD_7CwARw6arrk",
-    authDomain: "fire-auth2.firebaseapp.com",
-    databaseURL: "https://fire-auth2.firebaseio.com",
-    projectId: "fire-auth2",
-    storageBucket: "fire-auth2.appspot.com",
-    messagingSenderId: "587131087182",
-    appId: "1:587131087182:web:f7e53e9007fe7760d7561f"
-  };
+  apiKey: "AIzaSyB_3NNz6b0S_d4m8Eilicj0wJfZ3xJxqz8",
+  authDomain: "ema-john-e.firebaseapp.com",
+  databaseURL: "https://ema-john-e.firebaseio.com",
+  projectId: "ema-john-e",
+  storageBucket: "ema-john-e.appspot.com",
+  messagingSenderId: "916463269714",
+  appId: "1:916463269714:web:548625648eb6af02522c8b"
+};
   export default firebaseConfig;
