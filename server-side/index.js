baaa24930ed51bee5e766b0d6d9cc1480dff8250
mirtauhid <67647821+mirtauhid@users.noreply.github.com>
2020-10-09 01:46:54 +0600

    server updadated

--- server-side/index.js
+++ server-side/index.js
@@ -19,8 +19,10 @@ const port = 5000;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const productsCollection = client.db("emaJohnStore").collection("products");
-  const ordersCollection = client.db("emaJohnStore").collection("orders");
+    console.log(err);
+  const productsCollection = client.db("ema-john-store").collection("products");
+  const ordersCollection = client.db("ema-john-store").collection("orders");
+  console.log("db connected");
   
     app.post('/addProduct', (req, res) => {
         const products = req.body;
